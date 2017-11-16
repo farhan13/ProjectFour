2c1e7a047b58e084687eaa17da31da77403ac184
farhan13 <fahmed26@uic.edu>
2017-11-16 11:02:27 -0600

    4

--- Project4/src/Player1.java
+++ Project4/src/Player1.java
@@ -1,51 +1,53 @@
 import java.net.*;
 import java.util.ArrayList;
-import java.io.*; 
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
 
 public class Player1 extends Thread
-{ 
- private Socket clientSocket;
+{
+    private Socket clientSocket;
 
- public static void main(String[] args) throws IOException 
-   { 
-	
-    ServerSocket serverSocket = null; 
-    
-    // back-end stuff for server, create grid using 2d array
-    
-    int [][] board1 = new int[10][10];
-    
-    for (int x = 0; x<board1.length;x++){
-   	   for (int y = 0; y<board1.length;y++){
-   	      board1[x][y] = 0;  // Initialize the cell
-   	   }
-   	}
-    //System.out.println("abcdef");
-    
-  // /*  // check by printing board
-    
-    for ( int x = 0; x<10;x++){
-        String line = "";
-        for ( int y = 0; y <10;y++){
-            line+="["+board1[x][y]+"]";
+    public static void main(String[] args) throws IOException
+    {
+
+        ServerSocket serverSocket = null;
+
+        // back-end stuff for server, create grid using 2d array
+
+        int [][] board1 = new int[10][10];
+
+        for (int x = 0; x<board1.length;x++){
+            for (int y = 0; y<board1.length;y++){
+                board1[x][y] = 0;  // Initialize the cell
+            }
         }
-        System.out.println(line);
-    }
-    
-    
-    ArrayList ship_coordinates = new ArrayList<String_Coordinates>();
-    
-     
-   //  */
-   
-    // BOARD LOGIC
-    
+        //System.out.println("abcdef");
+
+        // /*  // check by printing board
+
+        for ( int x = 0; x<10;x++){
+            String line = "";
+            for ( int y = 0; y <10;y++){
+                line+="["+board1[x][y]+"]";
+            }
+            System.out.println(line);
+        }
+
+
+        ArrayList ship_coordinates = new ArrayList<String_Coordinates>();
+
+
+        //  */
+
+        // BOARD LOGIC
+
     /*	0 MEANS no ship, just water
      *  10 means Server ship/ player 1 ship 1 position
      *  20 means Server ship/ player 1 ship 2 position
-     *  30 means Server ship/ player 1 ship 3 position 
-     *  40 means Server ship/ player 1 ship 4 position 
-     *  50 means Server ship/ player 1 ship 5 position 
+     *  30 means Server ship/ player 1 ship 3 position
+     *  40 means Server ship/ player 1 ship 4 position
+     *  50 means Server ship/ player 1 ship 5 position
      *  2 means Player 2 ship/ client ship position
      *  3 means player 1 hit position
      *  4 means player 2 hit position
@@ -53,131 +55,136 @@ public class Player1 extends Thread
      *  6 = player 2 miss position
      * 	7 = player 1 ship + hit position
      *  8 = player 2 ship + hit position  // these 2 not useful
-     * 
+     *
      */
 
-    try { 
-         serverSocket = new ServerSocket(8080); 
-       //  System.out.println ("Connection Socket Created");		// not necessary
-         System.out.println ("New Socket open on Port: " +
-                             serverSocket.getLocalPort());
-         InetAddress addr = InetAddress.getLocalHost();
-         System.out.println("Java InetAddress localHost info: " + addr);
-         System.out.println("Local Host Name: " + addr.getHostName());
-         try { 
-              while (true)
-                 {
-                  System.out.println ("Waiting for Player2...");
-                  new Player1 (serverSocket.accept()); 
-    
-                  
-                 }
-             } 
-         catch (IOException e) 
-             { 
-              System.err.println("Accept failed."); 
-              System.exit(1); 
-             } 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("MSg 1: Could not listen on port: 8080."); 
-         System.exit(1); 
-        } 
-    finally
+        try {
+            serverSocket = new ServerSocket(8080);
+            //  System.out.println ("Connection Socket Created");		// not necessary
+            System.out.println ("New Socket open on Port: " +
+                    serverSocket.getLocalPort());
+            InetAddress addr = InetAddress.getLocalHost();
+            System.out.println("Java InetAddress localHost info: " + addr);
+            System.out.println("Local Host Name: " + addr.getHostName());
+            try {
+                while (true)
+                {
+                    System.out.println ("Waiting for Player2...");
+                    new Player1 (serverSocket.accept());
+
+
+                }
+            }
+            catch (IOException e)
+            {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+        }
+        catch (IOException e)
         {
-         try {
-              serverSocket.close(); 
-             }
-         catch (Exception e)
-             { 
-              System.err.println("MSG 2: Could not close port: 8080."); 
-              System.exit(1); 
-             } 
+            System.err.println("MSg 1: Could not listen on port: 8080.");
+            System.exit(1);
+        }
+        finally
+        {
+            try {
+                serverSocket.close();
+            }
+            catch (Exception e)
+            {
+                System.err.println("MSG 2: Could not close port: 8080.");
+                System.exit(1);
+            }
         }
 
-    
 
-	    
 
-    
-    
-    
-   }
 
- private Player1 (Socket clientSoc)
-   {
-    clientSocket = clientSoc;
-    System.out.println ("Socket open on Port: " +
-                             clientSocket.getLocalPort());
-    System.out.println ("Socket open on Port: " +
-                             clientSocket.getPort());
-    start();
-   }
 
- public void run()
-   {
-    System.out.println ("Players connected. Begin BattleShip!");
-    
-   
 
-    try { 
-         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), 
-                                      true); 
-         BufferedReader in = new BufferedReader( 
-                 new InputStreamReader( clientSocket.getInputStream())); 
 
-         String inputLine; 
-         int num = 1;
-         while ((inputLine = in.readLine()) != null) 
-             { 
-        	 
-              System.out.println ("client: " + inputLine); 
-             // out.println(inputLine.toUpperCase()); 
-              //out.println("[1,0]");			// testing
-              
-              in.read();
-              System.out.println("sever received:" + in.read());
-            //  System.out.println(in.readLine());
-              
-              if (inputLine.equals("End")) {			// not necessary
-                  break; 
-             } 
-              
-              System.out.println("...");
 
-   }
-         out.close(); 
-         in.close(); 
-         clientSocket.close(); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Problem with Communication Server");
-         System.exit(1); 
-        } 
     }
- 
- void sendShipPos () {		// send the positions of the ships for player1
-	 
-	 
-	 
- }
- 
- void sendFirePos () {		// send torpedo target location for player1
-	 
-	 
-	 
- }
+
+    private Player1 (Socket clientSoc)
+    {
+        clientSocket = clientSoc;
+        System.out.println ("Socket open on Port: " +
+                clientSocket.getLocalPort());
+        System.out.println ("Socket open on Port: " +
+                clientSocket.getPort());
+        start();
+    }
+
+    public void run()
+    {
+        System.out.println ("Players connected. Begin BattleShip!");
+
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+                    true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader( clientSocket.getInputStream()));
+
+            String inputLine;
+            int num = 1;
+            int x;
+            int i = 0;
+
+            while ((inputLine = in.readLine()) != null)
+//            while(i<20)
+            {
+
+               // System.out.println ("client: " + inputLine);
+                // out.println(inputLine.toUpperCase());
+                //out.println("[1,0]");			// testing
+                System.out.println ("Srart reading");
+//                x = Integer.toString(in.read());
+                System.out.println("sever received: " + inputLine);
+
+                int fsrhsn = parseInt(inputLine);
+                System.out.println("fsrhsn: " + fsrhsn);
+
+                //if (inputLine.equals("End")) {			// not necessary
+                //    break;
+                //}
+
+                System.out.println("...");
+                i++;
+
+            }
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Problem with Communication Server");
+            System.exit(1);
+        }
+    }
+
+    void sendShipPos () {		// send the positions of the ships for player1
+
+
+
+    }
+
+    void sendFirePos () {		// send torpedo target location for player1
+
+
+
+    }
 
 }
 
 
-// back-end stuff to figure out: send ship positions, send target location -> hit / miss 
+// back-end stuff to figure out: send ship positions, send target location -> hit / miss
 // convert to string, send via socket, receiver parses then retrieves data
 
-	    
 
-	    
+
+
 
 
--- Project4/src/Player2.java
+++ Project4/src/Player2.java
@@ -10,107 +10,109 @@ public class Player2 {
         BufferedReader in = null;
 
         int [][] board1 = new int[10][10];
-        
+
         for (int x = 0; x<board1.length;x++){
-       	   for (int y = 0; y<board1.length;y++){
-       	      board1[x][y] = 0;  // Initialize the cell
-       	   }
-       	}
-        
+            for (int y = 0; y<board1.length;y++){
+                board1[x][y] = 0;  // Initialize the cell
+            }
+        }
+
         board1[1][5] = 1;
         board1[1][6] = 1;
         board1[1][7] = 1;
-        
+
         String Player2Ship1 = "5,5";
-        
-        
-    	ArrayList<Coordinates> ship_coordinates = new ArrayList<Coordinates>();
-    	
+
+
+        ArrayList<Coordinates> ship_coordinates = new ArrayList<Coordinates>();
+
         for (int a = 0; a<board1.length;a++){
-        	   for (int b = 0; b<board1.length;b++){
-        	      
-        		   if(board1[a][b] == 1) {
-        			   
-        			   Coordinates coordinate = new Coordinates(a,b);
-        			   
-        			   ship_coordinates.add(coordinate);
-        			   
-        		   }     
-        	   }
-        	}
-        
-        ArrayList<String_Coordinates> ship_coordinates_s = new ArrayList<String_Coordinates>();
-        
-        
-        for (int i = 0; i<ship_coordinates.size(); i++){
-        	
-        	String_Coordinates string_coordinates = new String_Coordinates(Integer.toString(ship_coordinates.get(i).getX()), Integer.toString(ship_coordinates.get(i).getY()));
-        	
-        	ship_coordinates_s.add(string_coordinates);
+            for (int b = 0; b<board1.length;b++){
+
+                if(board1[a][b] == 1) {
+
+                    Coordinates coordinate = new Coordinates(a,b);
+
+                    ship_coordinates.add(coordinate);
+
+                }
+            }
         }
-        
+
+        ArrayList<String_Coordinates> ship_coordinates_s = new ArrayList<String_Coordinates>();
+
+
+        for (int i = 0; i<ship_coordinates.size(); i++){
+
+            String_Coordinates string_coordinates = new String_Coordinates(Integer.toString(ship_coordinates.get(i).getX()), Integer.toString(ship_coordinates.get(i).getY()));
+
+            ship_coordinates_s.add(string_coordinates);
+        }
+
         int size = ship_coordinates_s.size();
-        
+
         System.out.println("size of string-coordinates:" + size);			// testing
-        
-       // int ShipPositions = 
-        
-        
+
+        // int ShipPositions =
+
+
         try {
             // echoSocket = new Socket("taranis", 7);
             echoSocket = new Socket("127.0.0.1", 8080);
             out = new PrintWriter(echoSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
-                                        echoSocket.getInputStream()));
-            
+                    echoSocket.getInputStream()));
+
             DataOutputStream sending = new DataOutputStream(echoSocket.getOutputStream());
-            
-            
+
+
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host: Battleship.");
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for "
-                               + "the connection to: BattleShip");
+                    + "the connection to: BattleShip");
             System.exit(1);
         }
 
-	BufferedReader stdIn = new BufferedReader(
-                                   new InputStreamReader(System.in));
-	String userInput;
-	String ships;
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
+        String ships;
 
-	
+
 
         System.out.println ("Player 2 connected, enter any key to start Battleship");		// not necessary
-	while ((userInput = stdIn.readLine()) != null) 
-           {
-	    out.println(userInput);
-	    
-	    out.write(ship_coordinates_s.get(0).getX());  // 1
-	    out.write(ship_coordinates_s.get(0).getY());
-	    
-	    System.out.println("sending TO SERVER" + ship_coordinates_s.get(0).getX() +" " + ship_coordinates_s.get(0).getY());		//testing
-	    
-	    
+        //while ((userInput = stdIn.readLine()) != null)
+       int v = 0;
+//        while(v < 20)
+//        {
+            //out.println(userInput);
+
+            out.write(ship_coordinates_s.get(0).getX() + "\n");  // 1
+//            out.write(ship_coordinates_s.get(0).getY());
+
+            System.out.println("sending TO SERVER: " + ship_coordinates_s.get(0).getX() +" " + ship_coordinates_s.get(0).getY());		//testing
+
+
             // end loop
-            if (userInput.equals("End") )
-                break;
-            
-      
-            
-            
-	    System.out.println("Player2: " + in.readLine());			// not necessary
-	   }
-	
-	
-	
-	
-	
-	out.close();
-	in.close();
-	stdIn.close();
-	echoSocket.close();
+           // if (userInput.equals("End") )
+            //    break;
+
+
+
+
+//            System.out.println("Player2: " + in.readLine());			// not necessary
+  //      }
+
+
+
+
+
+        out.close();
+        in.close();
+        stdIn.close();
+        echoSocket.close();
     }
 }
 
